1214c508107c0e9811bc1529258e5e2cc126b4e6
un-lock-able <gaogao174561@outlook.com>
2024-02-20 22:12:16 +0800

    Display vote result

--- src/pages/GameList.vue
+++ src/pages/GameList.vue
@@ -11,6 +11,17 @@ export default {
     methods: {
         jumpToDetailOfGame(target_game_id) {
             this.$router.push("/game/" + target_game_id)
+        },
+        zeroPad(num) {
+            if (num < 10) {
+                return '' + 0 + num
+            }
+
+            return num
+        },
+        stringnifyTime(time_string) {
+            let time = new Date(time_string)
+            return time.getFullYear() + "/" + this.zeroPad(time.getMonth()) + "/" + this.zeroPad(time.getDate()) + " " + this.zeroPad(time.getHours()) + ":" + this.zeroPad(time.getMinutes())
         }
     },
     async created() {
@@ -18,22 +29,28 @@ export default {
         if (games.success) {
             this.games_list = games.games
         }
-        
     }
 }
 </script>
 
 <template>
     <el-container>
-        <el-header><div class="center-header">Moe Voting</div></el-header>
+        <el-header class="center-header">
+            <el-link :underline="false" @click="$router.push('/home')"><div class="header-size">Moe Voting</div></el-link>
+        </el-header>
         <el-main>
-            <el-table :data="games_list">
-                <el-table-column prop="game_name" label="游戏名"></el-table-column>
-                <el-table-column prop="last_update_time" label="信息更新时间"></el-table-column>
-                <el-table-column label="详细信息">
+            <el-table
+                :data="games_list"
+                :default-sort="{prop: 'last_update_time', order: 'descending'}"
+                stripe>
+                <el-table-column prop="game_name" label="游戏" sortable>
                     <template #default="scope">
-                        <!-- <el-button @click="jumpToDetailOfGame(scope.row.id)">Jump</el-button> -->
-                        <el-link @click="jumpToDetailOfGame(scope.row.id)" type="primary">查看</el-link>
+                        <el-link @click="jumpToDetailOfGame(scope.row.id)" type="primary">{{ scope.row.game_name }}</el-link>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="last_update_time" label="更新时间" sortable>
+                    <template #default="scope">
+                        {{ stringnifyTime(scope.row.last_update_time) }}
                     </template>
                 </el-table-column>
             </el-table>
--- src/pages/GameVoteResult.vue
+++ src/pages/GameVoteResult.vue
@@ -5,13 +5,70 @@
 export default {
     data() {
         return {
+            scores_list: [],
+            character_list: [],
+            game_name: "",
+        }
+    },
+    methods: {
+        getScore(row, column) {
+            var result = row.scores[column.property]
+            if (typeof(result) === 'undefined'){
+                return '-'
+            }
+            return result
+        }
+    },
+    async created() {
+        let scores = await fetch('/api/' + this.$route.params.gameId + '/result/').then((response) => response.json())
+        if (scores.success) {
+            this.scores_list = scores.vote_result
+        }
+
+        let characters = await fetch('/api/' + this.$route.params.gameId + '/characters/').then((response) => response.json())
+        if (characters.success) {
+            this.character_list = characters.characters
+            this.game_name = characters.game_name
         }
     }
 }
 </script>
 
 <template>
-{{ $route.params.gameId }}
+    <el-container>
+        <el-header class="center-header">
+            <el-link :underline="false" @click="$router.push('/home')"><div class="header-size">Moe Voting</div></el-link>
+        </el-header>
+        <el-main>
+            <el-page-header @back="this.$router.back()">
+                <template #content>{{ game_name }}</template>
+            </el-page-header>
+            <el-table :data="scores_list" stripe height="100%">
+                <el-table-column prop="participant_name" label="" fixed>
+                    <template #default="scope">
+                        <div class="no-text-wrap">
+                            {{ scope.row.participant_name }}
+                        </div>
+                    </template>
+                </el-table-column>
+                <el-table-column v-for="character in character_list" :label="character.name" :prop="character.name" :formatter="getScore">
+                    <template #header="scope">
+                        <div class="no-text-wrap">
+                            {{ scope.column.property }}
+                        </div>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <!-- {{ $route.params.gameId }}
+            {{ scores_list }}
+            {{ character_list }}
+            {{ game_name }} -->
+        </el-main>
+    </el-container>
 </template>
 
-<style scoped></style>
+<style scoped>
+.no-text-wrap {
+    text-wrap: nowrap;
+}
+</style>
