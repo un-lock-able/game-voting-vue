5de25705b79bd26ff57bbb5a1a654e90abd82bf2
un-lock-able <gaogao174561@outlook.com>
2024-02-21 14:52:29 +0800

    Update page title

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,23 @@ const routes = [
     {
         path: "/home",
         component: () => import("../pages/GameList.vue"),
+        meta: {
+            title: "Moe Voting"
+        },
     },
     {
         path: "/game/:gameId(\\d+)",
         component: () => import("../pages/GameVoteResult.vue"),
+        meta: {
+            title: "投票结果"
+        }
     },
     {
         path: "/game/:gameId(\\d+)/vote",
         component: () => import("../pages/Vote.vue"),
+        meta: {
+            title: "投票"
+        }
     },
 ];
 
